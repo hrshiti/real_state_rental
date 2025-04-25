4c080b21e65dab35cf412267fe97010fd6f31b47
hrshiti <hritik45raghuwanshi@gmail.com>
2025-04-25 10:52:58 +0530

    link updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const connectDB = require("./utils/db")
 
 
 const corsOptions ={
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://real-state-rental-front.onrender.com"],
     methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials:true,
 }
